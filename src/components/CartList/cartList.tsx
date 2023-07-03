cfaa7c7d674bcb37f09a6c5417e3b10061641fce
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-07-03 19:32:38 -0300

    feat: total added to shopping cart

--- src/components/CartList/cartList.tsx
+++ src/components/CartList/cartList.tsx
@@ -22,15 +22,25 @@ export default function CartList() {
     }, 3000);
   }, [getUserCart]);
 
+  const cartTotal = cart.reduce(
+    (acc: number, current) => (acc += current.quantity * current.product.price),
+    0
+  );
+
   return cart.length > 0 ? (
-    <motion.div
-      initial={{ opacity: 0, y: 50 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.5 }}
-      className="w-[100vw] flex flex-wrap mt-[30px] pt-[30px] h-[75vh] lg:h-[80vh] xl:h-[85vh] gap-8 justify-center content-start overflow-y-auto 3xl:w-[80vw] 3xl:m-margin-60-auto">
-      {cart.map((order) => {
-        return <CartCard key={order.id} order={order} />;
-      })}
+    <motion.div className="flex flex-col">
+      <motion.p className="w-[300px] self-center p-4 mr-4 bg-white-opacity40 rounded-lg text-center font-medium text-xl">
+        Total Carrinho: R$ {cartTotal}
+      </motion.p>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.5 }}
+        className="w-[100vw] flex flex-wrap mt-[30px] pt-[30px] h-[75vh] lg:h-[80vh] xl:h-[85vh] gap-8 justify-center content-start overflow-y-auto 3xl:w-[80vw] 3xl:m-margin-60-auto">
+        {cart.map((order) => {
+          return <CartCard key={order.id} order={order} />;
+        })}
+      </motion.div>
     </motion.div>
   ) : (
     <motion.div className="flex justify-center items-center text-4xl text-white h-[70vh] lg:h-[88vh]">
